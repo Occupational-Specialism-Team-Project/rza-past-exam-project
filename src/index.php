7bf3a1684cdd92aa1b45f584a87081c18e46afc3
Jayden Waghorn <dxmqkgax@duck.com>
2025-01-30 14:01:05 +0000

    Remove `carousel-dark` from carousel on index.php

--- src/index.php
+++ src/index.php
@@ -46,11 +46,11 @@ include_once "include/base.php";
                                 </div>
                             </div>
                         </div>
-                        <button class="carousel-control-prev carousel-dark" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Previous</span>
                         </button>
-                        <button class="carousel-control-next carousel-dark" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Next</span>
                         </button>
