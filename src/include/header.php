0faf42aa7dcecfd5478fb039649783a90dde546b
Jayden Waghorn <dxmqkgax@duck.com>
2025-02-01 21:28:14 +0000

    Create account dropdown for header navbar

--- src/include/header.php
+++ src/include/header.php
@@ -7,10 +7,10 @@
                 RZA
                 <i class="fa-solid fa-paw"></i>
             </a>
-            <button class="navbar-toggler fs-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler fs-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse fs-2" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse fs-3" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link<?php if (PAGE_TITLE == "Home Page"): echo " active"; endif ?>" <?php if (PAGE_TITLE == "Home Page"): echo "aria-current='page'"; endif ?> href="index.php">Home</a>
@@ -34,9 +34,32 @@
                         <a class="nav-link<?php if (PAGE_TITLE == "Attractions & Facilities"): echo " active"; endif ?>" <?php if (PAGE_TITLE == "Attractions & Facilities"): echo "aria-current='page'"; endif ?> href="index.php">Attractions & Facilities</a>
                     </li>
                 </ul>
-                <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                    <button class="btn btn-outline-success" type="submit">Search</button>
+                <div class="dropdown">
+                    <button class="btn <?=isset($_SESSION["user"]) ? "btn-success" : "btn-outline-secondary"?> dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="<?=isset($_SESSION["user"]) ? "fa-solid" : "fa-regular"?> fa-user"></i>
+                        <?=$_SESSION["user"] ?? "Not logged in"?>
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <?php if (! isset($_SESSION["user"])): ?>
+                            <li><a class="dropdown-item" href="#">
+                                <i class="fa-solid fa-right-from-bracket"></i>
+                                Log in
+                            </a></li>
+                            <li><a class="dropdown-item" href="#">
+                                <i class="fa-solid fa-user-plus"></i>
+                                Sign up
+                            </a></li>
+                        <?php else: ?>
+                            <li><a class="dropdown-item" href="#">
+                                <i class="fa-solid fa-gear"></i>
+                                Settings
+                            </a></li>
+                            <li><a class="dropdown-item" href="#">
+                                <i class="fa-solid fa-right-from-bracket"></i>
+                                Log out
+                            </a></li>
+                        <?php endif ?>
+                    </ul>
                 </div>
             </div>
         </div>
