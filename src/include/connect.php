8b6d020cacdeb385e3d9629fae62c5ba24eb58ab
Jayden Waghorn <dxmqkgax@duck.com>
2025-02-02 15:19:59 +0000

    Add reminder about database settings to connect.php

--- src/include/connect.php
+++ src/include/connect.php
@@ -1,6 +1,7 @@
 <?php
 
 try {
+    // Database settings can be changed in db_settings.ini
     $settings = parse_ini_file("db_settings.ini");
     $dsn = $settings["driver"] . ":host=". $settings['host'] . ";dbname=". $settings['dbname'];
 
