8f6c7b48caf3e41d2be0213d94e5daad89d85d7b
SpencerL334 <spanner2223@gmail.com>
2025-02-02 18:48:34 +0000

    added insert php helper function

--- src/sign_up.php
+++ src/sign_up.php
@@ -1,18 +1,35 @@
 <?php
 require_once "include/utils.php";
+function insertpdo($insertdataquery , $bind_parameter){
+    try{
+        global $pdo; 
+    
+        $stmt = $pdo->prepare($insertdataquery);
+        $stmt->execute($bind_parameter);
+        return $stmt ;
+    
+      }catch(PDOException $e){
+    
+        echo "insert error" .  $e->getMessage();
+       
+      }
+
+}
 if(isset($_POST['login'])){
     $username=$_POST['username'];
     $password=$_POST['password'];
     if(!empty($password)){
         $passwordhash= password_hash($password, PASSWORD_DEFAULT);
         if(!empty($username)){
-            $insertdata = "INSERT INTO users (username,password,role_name) VALUES(:username,:password,'customer')";
-             $stmt=$pdo->prepare($insertdata);
-             $stmt->execute(array(
-              ':username'=>$username,
-              ':password'=>$passwordhash,
-             ));
-             if($insertdata){
+            $insertdata = "INSERT INTO users (username,password,role_name) VALUES(:username ,:password ,'customer')";
+            $bind_parameter= array(
+                ':username' => $username ,
+                ':password' => $passwordhash
+            );
+             $result = insertpdo($insertdata, $bind_parameter);
+
+
+             if($result){
                  echo'<script> alert ("Data is updated")</script>';
               }else{
                  echo'<script> alert ("Data Not Updated")</script>';
@@ -45,27 +62,3 @@ include_once "include/base.php";
 </div>
 
 <?php include_once "include/footer.php";?>
-<!-- 
-<div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-</div> -->
-
-<!-- 
-<div class="container">
-    
-        <form method = "post" >
-            <h1  class="text-center">Riget Zoo Adventures</h1>
-            <h1 class="text-center">Sign Up</h1>
-            <input name ="username" type="text" class=" form-control mt-5 p-3 account-input "  placeholder="Enter your username" require>
-            <input name="password" type="password" class=" form-control mt-5 p-3 account-input " placeholder="Enter your password" require>
-            <button type="submit" name="login" class="mt-3 mx-auto col-2 rounded btn btn-success">submit</button>
-            <a href='login.php'><button type="button" class="mt-3 mx-auto col-2 rounded login-signup-button btn btn-primary">login</button></a>
-            
-        </form>
-    </div> -->
