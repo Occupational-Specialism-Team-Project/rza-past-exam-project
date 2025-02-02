71b0c9e2f3d3584f1f26d7348035a91d372b8ea1
Jayden Waghorn <dxmqkgax@duck.com>
2025-02-02 15:19:46 +0000

    Add CSS guide comments to base.php

--- src/include/base.php
+++ src/include/base.php
@@ -14,7 +14,13 @@ include_once "include/utils.php"
         <title><?=PAGE_TITLE?></title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <!-- Add extra <link> elements here, such as CSS -->
+        <!-- For example: -->
+
+        <!-- <link rel="stylesheet" href="style.css"> -->
+
         <style>
+            /* Add inline CSS here */
             .carousel-index-image {
                 min-height:25vh;
                 width: 100%;
