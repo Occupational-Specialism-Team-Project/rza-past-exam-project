7c92af8fbdee5ae14045f9613b2fa5373143015c
Jayden Waghorn <dxmqkgax@duck.com>
2025-02-02 17:01:23 +0000

    Update database dump with booking and visit tables

--- db/rza_project.sql
+++ db/rza_project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 24, 2025 at 10:51 PM
+-- Generation Time: Feb 02, 2025 at 06:00 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,16 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `hotel_bookings`
+--
+
+CREATE TABLE `hotel_bookings` (
+  `hotel_booking_id` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -33,14 +43,108 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`username`, `password`) VALUES
+('jayden123', '$2y$10$mNVJoTgJHL0FUEHo307VjOL3M.CHLnEQZeFtOoRW3wokQPnavmP46');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visits`
+--
+
+CREATE TABLE `visits` (
+  `visit_id` int(100) NOT NULL,
+  `zoo_booking_id` int(100) NOT NULL,
+  `hotel_booking_id` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `zoo_bookings`
+--
+
+CREATE TABLE `zoo_bookings` (
+  `zoo_booking_id` int(100) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `start_datetime` datetime NOT NULL,
+  `end_datetime` datetime NOT NULL,
+  `number_of_people` int(100) NOT NULL DEFAULT 1,
+  `educational_visit` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `hotel_bookings`
+--
+ALTER TABLE `hotel_bookings`
+  ADD PRIMARY KEY (`hotel_booking_id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `visits`
+--
+ALTER TABLE `visits`
+  ADD PRIMARY KEY (`visit_id`),
+  ADD KEY `visits_zoo_booking_id` (`zoo_booking_id`),
+  ADD KEY `visits_hotel_booking_id` (`hotel_booking_id`);
+
+--
+-- Indexes for table `zoo_bookings`
+--
+ALTER TABLE `zoo_bookings`
+  ADD PRIMARY KEY (`zoo_booking_id`),
+  ADD KEY `zoo_booking_username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `hotel_bookings`
+--
+ALTER TABLE `hotel_bookings`
+  MODIFY `hotel_booking_id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `visits`
+--
+ALTER TABLE `visits`
+  MODIFY `visit_id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `zoo_bookings`
+--
+ALTER TABLE `zoo_bookings`
+  MODIFY `zoo_booking_id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `visits`
+--
+ALTER TABLE `visits`
+  ADD CONSTRAINT `visits_hotel_booking_id` FOREIGN KEY (`hotel_booking_id`) REFERENCES `hotel_bookings` (`hotel_booking_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `visits_zoo_booking_id` FOREIGN KEY (`zoo_booking_id`) REFERENCES `zoo_bookings` (`zoo_booking_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `zoo_bookings`
+--
+ALTER TABLE `zoo_bookings`
+  ADD CONSTRAINT `zoo_booking_username` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
