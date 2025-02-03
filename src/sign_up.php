681866872d4f6b12cda35239975accead50cd214
Jayden Waghorn <dxmqkgax@duck.com>
2025-02-03 09:45:49 +0000

    Update card with proper and responsive layout

--- src/sign_up.php
+++ src/sign_up.php
@@ -48,19 +48,25 @@ include_once "include/base.php";
 
 
 
-<div class="card" style="width: 100%">
-    <form method = "post" >
-        <div class="card-header">
-        <h1  class="text-center card-title">Riget Zoo Adventures</h1>
-        <h1 class="text-center card-title">Sign Up</h1>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-4 mx-auto">
+            <div class="card" style="width: 100%">
+                <form method = "post" >
+                    <div class="card-header">
+                        <h1  class="text-center card-title">Riget Zoo Adventures</h1>
+                        <h1 class="text-center card-title">Sign Up</h1>
+                    </div>
+                    <div class="card-body">    
+                        <input name ="username" type="text" class=" form-control mt-5 p-3 account-input card-input"  placeholder="Enter your username" require>
+                        <input name="password" type="password" class=" form-control mt-5 p-3 account-input card-input " placeholder="Enter your password" require>
+                        <button type="submit" name="login" class="mt-3 mx-auto btn btn-success">submit</button>
+                        <a href='login.php'><button type="button" class="mt-3 mx-auto login-signup-button btn btn-primary">login</button></a>
+                    </div>
+                </form>
+            </div>
         </div>
-        <div class="card-body">    
-            <input name ="username" type="text" class=" form-control mt-5 p-3 account-input card-input "  placeholder="Enter your username" require>
-            <input name="password" type="password" class=" form-control mt-5 p-3 account-input card-input " placeholder="Enter your password" require>
-            <button type="submit" name="login" class="mt-3 mx-auto col-2 rounded btn btn-success card-button">submit</button>
-            <a href='login.php'><button type="button" class="mt-3 mx-auto col-2 rounded login-signup-button btn btn-primary card-button">login</button></a>
-        </div>
-    </form>
+    </div>
 </div>
 
 <?php include_once "include/footer.php";?>
