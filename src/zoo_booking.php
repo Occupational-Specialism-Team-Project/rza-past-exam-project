a071c72970743ec8066df58619d20eecb640d614
Jayden Waghorn <dxmqkgax@duck.com>
2025-02-05 21:42:03 +0000

    Create zoo booking form

--- src/zoo_booking.php
+++ src/zoo_booking.php
@@ -23,20 +23,41 @@ include_once "include/base.php";
         <section class="mx-auto col-md-4 mt-5">
             <div class="card">
                 <div class="card-header">
-                    <h1><?=PAGE_TITLE?></h1>
+                    <h1 class="text-center"><?=PAGE_TITLE?></h1>
                 </div>
-                <div class="card-body">
-                    <h2>Feeling adventurous? Book a visit to our zoo:</h2>
+                <form action="" method="POST" class="card-body container">
+                    <h2 class="text-center mb-4">Feeling adventurous? Book a visit to our zoo:</h2>
                     
-                    <p class="card-text">
-                        <?=$user?>
-                    </p>
-                    <p class="card-text">
-                        <?=$role?>
-                    </p>
+                    <div class="row mb-4">
+                        <div class="mb-3 col-md-6">
+                            <label for="start_datetime" class="form-label">Start date and time:</label>
+                            <input type="datetime-local" class="form-control" id="start_datetime" name="start_datetime" required>
+                        </div>
+                        <div class="mb-3 col-md-6">
+                            <label for="end_datetime" class="form-label">End date and time:</label>
+                            <input type="datetime-local" class="form-control" id="end_datetime" name="end_datetime" required>
+                        </div>
+                    </div>
+                    <div class="row mb-4">
+                        <div class="mb-3 col-md-8">
+                            <label for="number_of_people" class="form-label">Number of people:</label>
+                            <input type="number" class="form-control" id="number_of_people" name="number_of_people" required>
+                        </div>
+                        <div class="mb-3 col-md-4">
+                            <label for="educational_visit" class="form-label">Educational visit?</label>
+                            <br>
+                            <input type="checkbox" class="form-check-input" id="educational_visit" name="educational_visit">
+                        </div>
+                    </div>
 
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
+                    <div class="row mb-4">
+                        <button type="submit" class="btn btn-success float-end" id="book_ticket" name="book_ticket">Reserve a ticket</>
+                    </div>
+                    <div class="row">
+                        <?=var_dump($zoo_visit)?>
+                    </div>
+
+                </form>
             </div>
         </section>
     </div>
