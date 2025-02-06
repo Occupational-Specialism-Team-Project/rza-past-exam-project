ddb1c1cfae5bab3e6bbecd0435c637b84abcebda
Jayden Waghorn <dxmqkgax@duck.com>
2025-02-06 11:54:25 +0000

    Add partially-finished bookings query

--- src/get_booked_days.php
+++ src/get_booked_days.php
@@ -21,10 +21,43 @@ function get_booked_days($month_and_year, $number_of_people, $pdo) {
     #UNIX_TIMESTAMP()
 
     // "BETWEEN :first_day_of_month and :last_day_of_month" just means in that month
-    #$get_booked_days = $pdo->prepare("SELECT start_datetime, end_datetime, SUM(number_of_people) AS total_visitors
-    #                                  FROM zoo_bookings
-    #                                  WHERE (end_datetime BETWEEN :first_day_of_month AND :last_day_of_month)
-    #                                  OR (start_datetime BETWEEN :first_day_of_month AND :last_day_of_month)");
+    $get_bookings_intersecting_month = $pdo->prepare(
+        "SELECT start_datetime, end_datetime
+        FROM zoo_bookings
+        WHERE (
+            (end_datetime BETWEEN :first_day_of_month AND :last_day_of_month)
+            OR
+            (start_datetime BETWEEN :first_day_of_month AND :last_day_of_month)
+        )
+        "
+    );
+    $get_bookings_intersecting_month->execute([
+        "first_day_of_month" => $first_day_of_month,
+        "last_day_of_month" => $last_day_of_month
+    ]);
+    $bookings_intersecting_month = $get_bookings_intersecting_month->fetchAll();
+
+    foreach ($bookings_intersecting_month as $booking) {
+        // Define start and end dates
+        $startDate = new DateTime($booking["start_datetime"]);
+        $endDate = new DateTime($booking["end_datetime"]);
+        
+        // Create DatePeriod object
+        $interval = new DateInterval('P1D');
+        
+        // 1 day interval
+        $dateRange = new DatePeriod($startDate, $interval, $endDate->modify('+1 day'));
+        
+        // Collect dates in an array
+        $dates = [];
+        foreach ($dateRange as $date){
+            $dates[] = $date->format('Y-m-d');
+
+        }
+
+        var_dump($dates);
+        #$days_in_booking = [];
+    }
 
     // Should return some JSON like this
     // {
@@ -36,7 +69,7 @@ function get_booked_days($month_and_year, $number_of_people, $pdo) {
     //    "total_visitors" = 25
     //    ]
     // }
-    return;
+    return $booked_days;
 }
 
 $month_and_year = $_REQUEST["month"] ?? FALSE;
@@ -45,7 +78,7 @@ if (! $month_and_year) {
 }
 
 $month_and_year_value = strtotime($month_and_year);
-get_booked_days($month_and_year_value, 4, $pdo);
+#var_dump(get_booked_days($month_and_year_value, 4, $pdo));
 
 #echo "<br>";
 #echo cal_days_in_month(CAL_GREGORIAN, $month, $year);
