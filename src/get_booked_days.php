0b324b1694dccc5372a51fc2074c05491ff8892e
Jayden Waghorn <dxmqkgax@duck.com>
2025-02-06 14:34:04 +0000

    Update get_booked_days() with JOIN to daily bookings

--- src/get_booked_days.php
+++ src/get_booked_days.php
@@ -17,18 +17,29 @@ function get_booked_days($month_and_year, $number_of_people, $pdo) {
     // Substract 1 day from the days of the month because it will start on day 1, not day 0
     $last_day_of_month = date_modify(date_create($first_day_of_month), "+" . ($days_in_month) - 1 . " day")->format("Y-m-d H:i:s");
 
-    // "SELECT start_datetime, end_datetime, SUM(number_of_people) AS total_visitors
-    #UNIX_TIMESTAMP()
-
     // "BETWEEN :first_day_of_month and :last_day_of_month" just means in that month
     $get_bookings_intersecting_month = $pdo->prepare(
-        "SELECT start_datetime, end_datetime
-        FROM zoo_bookings
+        "SELECT
+            zoo_bookings_daily.zoo_booking_daily_id, # Needed for booking
+            zoo_bookings_daily.zoo_booking_id, # Needed for INNER JOIN
+            zoo_bookings.zoo_booking_id, # Needed for INNER JOIN
+            zoo_bookings.start_datetime, zoo_bookings.end_datetime # Needed to only SELECT BETWEEN a certain month
+        FROM zoo_bookings_daily
+        JOIN zoo_bookings
+        ON zoo_bookings.zoo_booking_id = zoo_bookings_daily.zoo_booking_id
         WHERE (
             (end_datetime BETWEEN :first_day_of_month AND :last_day_of_month)
             OR
             (start_datetime BETWEEN :first_day_of_month AND :last_day_of_month)
         )"
+
+        #"SELECT zoo_booking_id, start_datetime, end_datetime
+        #FROM zoo_bookings
+        #WHERE (
+        #    (end_datetime BETWEEN :first_day_of_month AND :last_day_of_month)
+        #    OR
+        #    (start_datetime BETWEEN :first_day_of_month AND :last_day_of_month)
+        #)"
     );
     $get_bookings_intersecting_month->execute([
         "first_day_of_month" => $first_day_of_month,
@@ -46,7 +57,7 @@ function get_booked_days($month_and_year, $number_of_people, $pdo) {
     //    "total_visitors" = 25
     //    ]
     // }
-    return $booked_days;
+    return $bookings_intersecting_month;
 }
 
 $month_and_year = $_REQUEST["month"] ?? FALSE;
@@ -55,7 +66,9 @@ if (! $month_and_year) {
 }
 
 $month_and_year_value = strtotime($month_and_year);
-#var_dump(get_booked_days($month_and_year_value, 4, $pdo));
+echo "<pre>";
+print_r(get_booked_days($month_and_year_value, 4, $pdo));
+echo "</pre>";
 
 #echo "<br>";
 #echo cal_days_in_month(CAL_GREGORIAN, $month, $year);
