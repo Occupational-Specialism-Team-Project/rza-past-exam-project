d1f986a8b8ba5d793ebe1872b3cd981da0d8719c
Jayden Waghorn <dxmqkgax@duck.com>
2025-02-07 17:08:51 +0000

    Make jQuery CDN and calendar.js only a dependency of zoo booking system

--- src/include/footer.php
+++ src/include/footer.php
@@ -55,8 +55,10 @@
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js" integrity="sha512-2rNj2KJ+D8s1ceNasTIex6z4HWyOnEYLVC3FigGOmyQCZc2eBXKgOxQmo3oKLHyfcj53uz4QMsRCWNbLd32Q1g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-        <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
-        <script src="include/calendar.js"></script>
+        <?php if (basename($_SERVER["PHP_SELF"]) == "zoo_booking.php"): ?>
+            <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+            <script src="include/calendar.js"></script>
+        <?php endif ?>
         <!-- Add extra JavaScript here -->
     </body>
 </html>
